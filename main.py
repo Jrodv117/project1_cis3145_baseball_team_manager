5db5c2bd3e25ab8a4b068e16a30975a6bad0ea08
jrodr117 <jrodr113@msudenver.edu>
2022-10-02 16:16:42 -0600

    Made main functino to call the menu that prints program options

---
+++ main.py
@@ -0,0 +1,23 @@
+def menu():
+    print("++++++++++++++++++++++++++++++++++++++++++++++")
+    print("Baseball Team Management Program")
+    print("MENU OPTIONS")
+    print(
+        "1 - Display lineup\n"
+        + "2 - Add player\n"
+        + "3 - Remove player\n"
+        + "4 - Move player\n"
+        + "5 - Edit player position\n"
+        + "6 - Edit player stats \n"
+        + "7 - Exit program"
+    )
+
+    print("++++++++++++++++++++++++++++++++++++++++++++++/n")
+    print()
+
+
+def main():
+    menu()
+
+
+main()
